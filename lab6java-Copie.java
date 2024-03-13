918204e3db95e544f04aa57ffcf958cfa216bd80
Samuel Levy <samuelevy18@gmail.com>
2024-03-13 10:04:56 +0000

    Added a copy

---
+++ lab6java-Copie.java
@@ -0,0 +1,11 @@
+class Hello{
+	public static void world(){
+		System.out.println("Hello World!");
+		return;
+	}
+    public static void main (String[] args)
+    {
+        world();
+        return;
+    }
+}
